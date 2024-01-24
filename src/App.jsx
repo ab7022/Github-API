8605f62e35471c8d1e2d08c0106e2c921a1b601b
ab7022 <bayees1@gmail.com>
2024-01-24 21:20:21 +0530

    removed unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,60 @@
-import React, { useState } from "react";
-import axios from "axios";
-import UserData from "./UserData";
-import "./App.css";
+import axios from 'axios';
+import UserData from './UserData'; 
+import { useState } from 'react';
+import './App.css';
 
-function App() {
+export default function App() {
   const [data, setData] = useState({});
   const [username, setUsername] = useState("");
   const [error, setError] = useState(null);
   const [showErrorMessage, setShowErrorMessage] = useState(false);
-  const token = "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";
+
+  const token = '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';
   const url = `https://api.github.com/users/${username}`;
-  const fetchData = async () => {
+  console.log(url)
+  const fetchData = async (e) => {
+    e.preventDefault()
     try {
-      setData({});
-      setError(null);
-      setShowErrorMessage(false);
       const response = await axios.get(url, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
-      });
+      }); 
       setData(response.data);
+      setError(null); 
+      setShowErrorMessage(false)  
     } catch (error) {
-      setData({});
+      setData({})
       setShowErrorMessage(true);
-      setError("Invalid GitHub username. Please check and try again.");
-      console.error("Error fetching GitHub data:", error);
+
+      setError('Invalid GitHub username. Please check and try again.');
+      console.error('Error fetching GitHub data:', error);
     }
   };
+
   return (
     <div>
-      <div id="maindiv">
-        <h1>Github User Data</h1>
-        <div className="action">
-          <input
-            type="text"
-            name="id"
-            id="inputbox"
-            value={username}
-            placeholder="Enter Github Username"
-            onChange={(e) => {
-              setUsername(e.target.value.trim());
-            }}
-          />
-          <button onClick={() => fetchData()}>Search</button>
-          {error && <p className="error-message">{error}</p>}
-          <UserData data={data} />
-        </div>
-      </div>
+      <div id='maindiv'>
+      <h1>Github User Data</h1>
+      <form action='/' method='post' onSubmit={fetchData}>
+         <input
+        type="text"
+        name="githubid"
+        id="inputbox"
+        value={username}
+        placeholder='Enter Github Username'
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
+      <button type='submit'>Search</button>
+      </form>
+     
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <UserData data={data} />
+      
     </div>
+    </div>
+    
   );
 }
-
-export default App;
